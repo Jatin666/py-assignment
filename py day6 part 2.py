c9df5cec5395d3fafb85f31bba2a6e0c6207b78f
Jatin666 <59907081+Jatin666@users.noreply.github.com>
2020-02-03 12:03:38 +0530

    day 6 part2 completed

---
+++ py day6 part 2.py
@@ -0,0 +1,84 @@
+
+
+EXECUTE THE COMMAND PRINT "DOUBLE QUOTES"
+
+PRINT "SINGLE QUOTES"
+
+PRINT ''' MULTILINE'''
+
+STRING1='HELLO WORLD'
+
+STRING2= "WE'RE HERE#1!"
+
+STRING3='1234'
+
+STRING4="I SAID, "PULL IT OVER BY THE LLAMMA"
+
+NUMBER = 12
+
+FLOAT = 3.14
+
+##PRINTING WITH DOUBLE QUOTES
+print ("DOUBLE QUOTES")
+DOUBLE QUOTES
+##printing with single quotes
+print('single quotes')
+single quotes
+##printing with triple quotes in multiple line
+print('''
+hii
+i m
+using multiline''')
+hii
+i m
+using multiline
+
+##printing a string with single quotes
+string1='HELLO WORLD'
+print(string1)
+HELLO WORLD
+##printing a string containing numbers
+string2='1234'
+print(string2)
+1234
+
+##printing a string containing "WE'RE HERE#1!
+string3="WE'RE HERE#1!"
+print(string3)
+WE'RE HERE#1!
+
+##printing a string containing 'I SAID, "PULL IT OVER BY THE LLAMMA"'
+string4=''' 'I SAID, "PULL IT OVER BY THE LLAMMA"' '''
+print(string4)
+ 'I SAID, "PULL IT OVER BY THE LLAMMA"'
+##printing a number12
+number= 12
+print(number)
+12
+##printing a float 3.14
+float= 3.14
+print(float)
+3.14
+
+
+##USING TYPE() TO CHECK THE DATATYPE OF THE CREATED VARIABLE
+
+
+age = 3.14
+print( type( age ))
+In [0]:
+
+##EXECUTE THE FOLLOWING
+
+MOVIES=['AVENGER','SPIDERMAN','SUPERMAN']
+
+MOVIES=('AVENGER','SPIDERMAN','SUPERMAN')**
+
+
+##PRINTING THE CODE USING LIST AND TUPLE
+movies_list=['AVENGER','SPIDERMAN','SUPERMAN']
+movies_tuple=('AVENGER','SPIDERMAN','SUPERMAN')
+print(movies_list)
+print(movies_tuple)
+['AVENGER', 'SPIDERMAN', 'SUPERMAN']
+('AVENGER', 'SPIDERMAN', 'SUPERMAN')
