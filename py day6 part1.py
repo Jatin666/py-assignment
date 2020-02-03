621908bf53541cd6819727e234fd0929f9ee23b3
Jatin666 <59907081+Jatin666@users.noreply.github.com>
2020-02-03 11:52:35 +0530

    day 6 part1 completed

---
+++ py day6 part1.py
@@ -0,0 +1,60 @@
+
+##PRINTING A PATTERN WITH * USING PRINT COMMAND
+print('''         ********************
+         ********************
+         ********************
+         ********************''')
+         ********************
+         ********************
+         ********************
+         ********************
+
+##PRINTING A PATTERN USING PRINT COMMAND
+print('''
+***********
+
+*         *
+
+*         *
+
+***********''')
+***********
+
+*         *
+
+*         *
+
+***********
+
+##PRINTING A TRAINGLE USING PRINT COMMAND
+print('''
+*
+**
+***
+****
+*****''')
+##ASK THE USER TO INPUT THE NAME PHONE NUMBER AND EMAIL ID.ONCE DETAILS ARE TAKEN INSERT THE DETAIL IN THE LIST AND PRINT THE LIST
+
+
+a=input("ENTER YOUR NAME : ")
+b=int(input("ENTER YOUR PHONE_NUMBER : "))
+C=input("ENTER YOUR EMAIL ID : ")
+LIST=['a','b','c']
+print(a)
+print(b)
+print(C)
+ENTER YOUR NAME : Jatin
+ENTER YOUR PHONE_NUMBER : 8872103661
+ENTER YOUR EMAIL ID : verma.jatin661@gmail.com
+Jatin
+8872103661
+verma.jatin661@gmail.com
+
+print("hii this code is used to double your word : ")
+##write a program that ask the user to enter the string and create a new string that double each character of the original string. for example . if the user enter HELLO the answer will be HHEELLLLOO.
+
+a=input("ENTER A STRING : ")
+print(a[0]*2)
+hii this code is used to double your word :
+ENTER A STRING : h
+hh
